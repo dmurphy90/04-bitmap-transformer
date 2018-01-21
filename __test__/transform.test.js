3d0aa51490ed588a5e2331a41e64e0bbf66ba0ed
Dean <dean.murphy90@gmail.com>
2018-01-20 17:07:52 -0800

    writing tests, they don't work

--- __test__/transform.test.js
+++ __test__/transform.test.js
@@ -1,33 +1,38 @@
 'use strict';
 
 const transform = require('../lib/transform.js');
+const bmp = require('../lib/bitmap.js');
+const fs = require('fs');
 require('jest');
+let testBmp = new bmp(fs.readFile('../assets/bitmap.bmp'));
+let flippedBmp = new bmp(fs.readFile('../assets/flipmap.bmp'));
+let shuffledBmp = new bmp(fs.readFile('../assets/shuffle.bmp'));
 
 describe('Transform Module', () => {
   describe('#flipImage', () => {
     it('should make sure an argument is passed in', () => {
       expect(transform.flipImage()).toBe(null);
     });
-    it('should make sure the argument passed in is an array', () => {
-      expect(transform.flipImage({})).toBe(null);
+    it('should make sure the argument passed in is an object', () => {
+      expect(transform.flipImage([])).toBe(null);
       expect(transform.flipImage('dog')).toBe(null);
       expect(transform.flipImage(90)).toBe(null);
     });
-    it('should make sure the output array is not the same as the input', () => {
-      expect(transform.flipImage({pixelArray: [1, 5, 7]})).not.toBe({pixelArray: [1, 5, 7]});
+    it('should make sure the output key/value is not the same as the input', () => {
+      expect(testBmp).not.toEqual(flippedBmp);
     });
   });
   describe('#shuffle', () => {
     it('should make sure an argument is passed in', () => {
-      expect(transform.shuffle()).toBe(null);
+      expect(transform.shuffle('asdfasdf')).toBe(null);
     });
-    it('should make sure the argument passed in is an array', () => {
-      expect(transform.shuffle({})).toBe(null);
+    it('should make sure the argument passed in is an object', () => {
+      expect(transform.shuffle([])).toBe(null);
       expect(transform.shuffle('dog')).toBe(null);
       expect(transform.shuffle(90)).toBe(null);
     });
-    it('should make sure the output array is not the same as the input', () => {
-      expect(transform.flipImage({pixelArray: [2, 4, 6, 8, 10]})).not.toBe({pixelArray: [2, 4, 6, 8, 10]});
+    it('should make sure the output key/value is not the same as the input', () => {
+      expect(testBmp).not.toEqual(shuffledBmp);
     });
   });
 });
