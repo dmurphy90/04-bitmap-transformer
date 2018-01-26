66cb6815c70d42c738fe2459716841eccea689fb
Joel Clair <clair.joel@yahoo.com>
2018-01-25 22:47:37 -0800

    clean up

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-const Bmp = require('./lib/bitmap.js');
-const transform = require('./lib/transform.js');
-const reader = require('./lib/reader.js');
-
-
-reader.readWrite('../assets/bitmap.bmp', '../assets/invertColorMap.bmp', 'invertColor');
